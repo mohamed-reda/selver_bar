a95195dc1f63308adeeefe8acb83db5700fa8502
Mohamed Reda <mohamed@wrangleapp.io>
2020-10-28 22:18:20 +0200

    finish the required UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,8 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  var scrollController = ScrollController();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,64 +18,53 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatefulWidget {
+//  ScrollController scrollController = ScrollController();
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  String url =
+      "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
   @override
   Widget build(BuildContext context) {
-    String url =
-        "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          DefaultTabController(
-            length: 2,
-            child: NestedScrollView(
-              headerSliverBuilder:
-                  (BuildContext context, bool innerBoxIsScrolled) {
-                return [
-                  SliverAppBar(
-                    expandedHeight: 300.0,
-                    floating: true,
-                    pinned: true,
-                    snap: false,
-                    actionsIconTheme: IconThemeData(opacity: 0.0),
-                    flexibleSpace: Stack(
-                      children: <Widget>[
-                        Positioned.fill(
-                          child: Image.network(
-                            url,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ];
-              },
-              body: Stack(
-                children: <Widget>[
-                  Image.network(
-                    url,
-                    fit: BoxFit.fitWidth,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(50),
-                          topRight: Radius.circular(50),
-                        ),
-                        color: Colors.green),
-                    child: Center(
-                      child: Text("Good morning Braken"),
-                    ),
-                  ),
-                ],
-              ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * .4 + 50,
+            child: Image.network(
+              url,
+              fit: BoxFit.cover,
             ),
           ),
+          Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .4,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * .6,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50),
+                    ),
+                    color: Colors.green),
+                child: Center(
+                  child: Text("Good morning Braken"),
+                ),
+              ),
+            ],
+          ),
+
         ],
       ),
     );
